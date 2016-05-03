550c5d6196b1aa931eeff5bae0ad8e7ed5ce8032
Leo Dias <leodiass@gmail.com>
2016-05-03 18:09:17 -0300

    fourth commit

--- BoasPraticasOOP/Models/Especialidade.cs
+++ BoasPraticasOOP/Models/Especialidade.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
 
 namespace BoasPraticasOOP.Models
 {
@@ -11,8 +10,8 @@ namespace BoasPraticasOOP.Models
     {
         public int IDEspecialidade { get; set; }
 
-        [Required(ErrorMessage = "O campo Nome é de preenchimento obrigatório.")]
-        [StringLength(80, ErrorMessage = "O campo Nome deve possuir no máximo 100 caracteres.")]
+        //[Required(ErrorMessage = "O campo Nome é de preenchimento obrigatório.")]
+        //[StringLength(80, ErrorMessage = "O campo Nome deve possuir no máximo 100 caracteres.")]
         public int Nome { get; set; }
     }
 }
--- BoasPraticasOOP/Models/Medico.cs
+++ BoasPraticasOOP/Models/Medico.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
 
 namespace BoasPraticasOOP.Models
 {
@@ -13,33 +12,33 @@ namespace BoasPraticasOOP.Models
         public int IDCidade { get; set; }
         public int IDEspecialidade { get; set; }
 
-        [Required(ErrorMessage = "O campo CRM é de preenchimento obrigatório.")]
-        [StringLength(30, ErrorMessage = "O campo CRM deve possuir no máximo 30 caracteres.")]
+        //[Required(ErrorMessage = "O campo CRM é de preenchimento obrigatório.")]
+        //[StringLength(30, ErrorMessage = "O campo CRM deve possuir no máximo 30 caracteres.")]
         public string CRM { get; set; }
 
-        [Required(ErrorMessage = "O campo Nome é de preenchimento obrigatório.")]
-        [StringLength(80, ErrorMessage = "O campo Nome deve possuir no máximo 100 caracteres.")]
+        //[Required(ErrorMessage = "O campo Nome é de preenchimento obrigatório.")]
+        //[StringLength(80, ErrorMessage = "O campo Nome deve possuir no máximo 100 caracteres.")]
         public string Nome { get; set; }
 
-        [Required(ErrorMessage = "O campo Endereço é de preenchimento obrigatório.")]
-        [StringLength(100, ErrorMessage = "O campo Endereço deve possuir no máximo 100 caracteres.")]
+        //[Required(ErrorMessage = "O campo Endereço é de preenchimento obrigatório.")]
+        //[StringLength(100, ErrorMessage = "O campo Endereço deve possuir no máximo 100 caracteres.")]
         public string Endereco { get; set; }
 
-        [Required(ErrorMessage = "O campo Bairro é de preenchimento obrigatório.")]
-        [StringLength(60, ErrorMessage = "O campo Bairro deve possuir no máximo 100 caracteres.")]
+        //[Required(ErrorMessage = "O campo Bairro é de preenchimento obrigatório.")]
+        //[StringLength(60, ErrorMessage = "O campo Bairro deve possuir no máximo 100 caracteres.")]
         public string Bairro { get; set; }
 
-        [Required(ErrorMessage = "O campo E-mail é de preenchimento obrigatório.")]
-        [StringLength(100, ErrorMessage = "O campo E-mail deve possuir no máximo 100 caracteres.")]
+        //[Required(ErrorMessage = "O campo E-mail é de preenchimento obrigatório.")]
+        //[StringLength(100, ErrorMessage = "O campo E-mail deve possuir no máximo 100 caracteres.")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "O campo Atende Por Convênio é de seleção obrigatória.")]
+        //[Required(ErrorMessage = "O campo Atende Por Convênio é de seleção obrigatória.")]
         public bool AtendePorConvenio { get; set; }
 
-        [Required(ErrorMessage = "O campo Tem Clínica é de seleção obrigatória.")]
+        //[Required(ErrorMessage = "O campo Tem Clínica é de seleção obrigatória.")]
         public bool TemClinica { get; set; }
 
-        [StringLength(80, ErrorMessage = "O campo Nome deve possuir no máximo 100 caracteres.")]
+        //[StringLength(80, ErrorMessage = "O campo Nome deve possuir no máximo 100 caracteres.")]
         public string WebsiteBlog { get; set; }
     }
 }
